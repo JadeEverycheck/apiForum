1a5f0ef1d8d6f72232f3824c4a73badc36bee719
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-16 17:17:10 +0200

    correct hostname

--- static/js/host.js
+++ static/js/host.js
@@ -2,7 +2,7 @@
 let host = localStorage.getItem('host')
 console.log('host : ', host)
 if (host == null) {
-	localStorage.setItem('host', "https://serene-shore-48766.herokuapp.com")
+	localStorage.setItem('host', "https://boiling-waters-46327.herokuapp.com")
 	// localStorage.setItem('host', "http://localhost:8080")
 	host = localStorage.getItem('host')
 }
