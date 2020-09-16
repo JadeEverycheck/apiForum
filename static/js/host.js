3f056888e3389f505e4f3013d78f1a548d2127b8
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-16 16:21:44 +0200

    try host

--- static/js/host.js
+++ static/js/host.js
@@ -1,6 +1,6 @@
 
 let host = localStorage.getItem("host")
-
+console.log('host : ', host)
 if (host == null) {
 	localStorage.setItem('host', "https://serene-shore-48766.herokuapp.com")
 	// localStorage.setItem('host', "http://localhost:8080")
