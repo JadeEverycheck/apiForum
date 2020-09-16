ed226eb218b771ae447786664d1327f95e057be8
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-09-16 16:26:29 +0200

    try host

--- static/js/host.js
+++ static/js/host.js
@@ -1,9 +1,9 @@
 
-let host = localStorage.getItem("host")
+let host = localStorage.getItem('host')
 console.log('host : ', host)
 if (host == null) {
 	localStorage.setItem('host', "https://serene-shore-48766.herokuapp.com")
 	// localStorage.setItem('host', "http://localhost:8080")
-	host = localStorage.getItem("host")
+	host = localStorage.getItem('host')
 }
 
