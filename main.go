b3010919cdf53c56b3f76bff408f4a9d299869b5
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-10-22 17:35:06 +0200

    try debug

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 	FileServer(r, "/static", filesDir)
 
 	workDir, _ = os.Getwd()
-	filesDir = http.Dir(filepath.Join(workDir, "react/my-app-react/build"))
+	filesDir = http.Dir(filepath.Join(workDir, "react/my-app-react/public"))
 	FileServer(r, "/", filesDir)
 
 	db.AutoMigrate(&api.User{})
