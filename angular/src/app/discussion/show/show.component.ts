6e534de8113ce443d65db3228afc8522822697b1
Jade Scarmoncin <jade.scarmoncin@everycheck.fr>
2020-10-26 15:43:54 +0100

    add show a discussion

--- angular/src/app/discussion/show/show.component.ts
+++ angular/src/app/discussion/show/show.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
+
+class Message {
+	user:string;
+	content:string;
+	date:string;
+	constructor(user:string, content:string, date:Date) {
+		this.user = user;
+		this.content = content;
+		this.date = moment(date).locale("fr").format("LLL");
+	}
+}
 
 @Component({
 	selector: 'app-show',
 	templateUrl: './show.component.html',
 	styleUrls: ['./show.component.css']
 })
+
 export class ShowComponent implements OnInit {
 
 	id : number = 0; 
+	items:Array<Message> = [];
 
 	constructor(
     private route: ActivatedRoute,
@@ -18,6 +32,9 @@ export class ShowComponent implements OnInit {
 	ngOnInit(): void {
 		let stringId:string = this.route.snapshot.paramMap.get('id');
 		this.id = parseInt(stringId);
+		for(let i=0;i<5;i++){
+			this.items.push(new Message("user","test ", new Date()));
+		}
 	}
 
 }
